a58c8527f4fd28d24c264ab7b9a535d4d5e31f6d
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-03-25 19:17:23 +0200

    server setup

---
+++ http/simple-web/server.js
@@ -0,0 +1,34 @@
+const http = require('node:http');
+
+const fs = require('node:fs/promises');
+
+const server = http.createServer()
+
+
+server.on("request",async (req, res) => {
+ if (req.url === '/' && req.method === 'GET'){
+  res.setHeader('Content-Type', 'text/html');
+
+  const fileHandle = await fs.open("./public/index.html","r")
+  const fileStream = fileHandle.createReadStream();
+
+  fileStream.pipe(res)
+
+ }
+
+
+ if(req.url === '/style.css' && req.method === 'GET'){
+   res.setHeader('Content-Type', 'text/css');
+
+   const fileHandle = await fs.open("./public/style.css","r")
+   const fileStream = fileHandle.createReadStream();
+
+   fileStream.pipe(res)
+
+  }
+
+})
+
+server.listen(9000,()=>{
+  console.log('Web server is live at http://localhost:9000')
+})
