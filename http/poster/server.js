dab92407d445b890ccde0c2f8c202541ed940054
Ali-Adel-Nour <aliiiadel841@gmail.com>
2024-08-05 20:59:30 +0300

    making middleware

--- http/poster/server.js
+++ http/poster/server.js
@@ -28,6 +28,29 @@ const PORT = 9001
 
 const server = new Ali()
 
+//Middleware
+
+server.beforeEach((req,res,next)=>{
+  console.log("This is the first middleware function")
+
+  next()
+})
+
+
+server.beforeEach((req,res)=>{
+
+
+  setTimeout(()=>{
+    console.log("This is the sec middleware function")
+    next()
+  },2000)
+})
+
+
+server.beforeEach((req,res)=>{
+  console.log("This is the third middleware function")
+  next()
+})
 
 
 //File Routes
@@ -58,66 +81,63 @@ server.route("get", '/scripts.js', (req, res) => {
 //Json Routes
 
 
-server.route("post", '/api/login', (req, res) => {
-
-  let body = ""
-
+server.route("post", "/api/login", (req, res) => {
+  let body = "";
   req.on("data", (chunk) => {
-    body += chunk.toString("utf-8")
-  })
+    body += chunk.toString("utf-8");
+  });
+
   req.on("end", () => {
-    body = JSON.parse(body)
+    body = JSON.parse(body);
 
-    const username = body.username
-    const password = body.password
+    const username = body.username;
+    const password = body.password;
 
-    const user = USERS.find((user) => user.username === username)
+    // Check if the user exists
+    const user = USERS.find((user) => user.username === username);
 
-
+    // Check the password if the user was found
     if (user && user.password === password) {
+      // At this point, we know that the client is who they say they are
 
+      // Generate a random 10 digit token
+      const token = Math.floor(Math.random() * 10000000000).toString();
 
-      const token = Math.floor( Math.random( ) * 1000000000000000).toString()
+      // Save the generated token
+      SESSIONS.push({ userId: user.id, token: token });
 
-      //Save The Genrated Token
-
-      SESSIONS.push( { userId:user.id, token:token } )
-
-      res.setHeader("Set-Cookie", `token=${token}; Path=/;`)
-
-
-      res.status(200).json({ message: "Login Successful" })
-
+      res.setHeader("Set-Cookie", `token=${token}; Path=/;`);
+      res.status(200).json({ message: "Logged in successfully!" });
     } else {
-      res.status(401).json({ message: "Login Failed" })
+      res.status(401).json({ error: "Invalid username or password." });
     }
-  })
-})
+  });
+});
 
+// Log a user out
+server.route("delete", "/api/logout", (req, res) => {});
 
-server.route("get","/api/user",(req,res)=>{
+// Send user info
+server.route("get", "/api/user", (req, res) => {
+  const token = req.headers.cookie.split("=")[1];
 
-  const token = req.headers.cookie.split("=") [1]
-
-  const session = SESSIONS.find((session) => session.token === token)
-
-  if (session){
-console.log("Sending the user info......")
-  }else{
-    res.status(401).json({ error: "Unauthorized" })
+  const session = SESSIONS.find((session) => session.token === token);
+  if (session) {
+    // Send the user's profile info
+    const user = USERS.find((user) => user.id === session.userId);
+    res.json({ username: user.username, name: user.name });
+  } else {
+    res.status(401).json({ error: "Unauthorized" });
   }
-  console.log(token)
-})
+});
 
-//send all post routes I have
+// Update a user info
+server.route("put", "/api/user", (req, res) => {});
+
+// Send the list of all the posts that we have
 server.route("get", "/api/posts", (req, res) => {
-
-
-
-
-  const posts = POSTS.map(post => {
-    const user = USERS.find(user => user.id === post.userId);
-
+  const posts = POSTS.map((post) => {
+    const user = USERS.find((user) => user.id === post.userId);
     post.author = user.name;
     return post;
   });
@@ -125,8 +145,9 @@ server.route("get", "/api/posts", (req, res) => {
   res.status(200).json(posts);
 });
 
-
+// Create a new post
+server.route("post", "/api/posts", (req, res) => {});
 
 server.listen(PORT, () => {
-  console.log("Server has started on port " + PORT)
+  console.log(`Server has started on port ${PORT}`);
 });
